a639aaed1adf94fbf09bf95fa390c431a788de62
Aronnam117 <mateoaronna@hotmail.com>
2024-03-10 16:18:31 -0500

    fist commit

---
+++ app.js
@@ -0,0 +1,92 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const mysql = require('mysql2');
+
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http, {
+  path: '/socket.io',
+});
+
+app.use(cors());
+
+// Crear el servidor UDP
+const dgram = require('dgram');
+const udpServer = dgram.createSocket('udp4');
+
+const db = mysql.createPool({
+  host: '%%',
+  user: '%%',
+  password: '%%',
+  database: '%%',  
+  table: '%%',    
+});
+
+
+const ultimaInformacion = {
+  latitud: 0,
+  longitud: 0,
+  fecha: '',
+  hora: '',
+};
+
+udpServer.on('message', (msg, rinfo) => {
+  const data = msg.toString();
+  console.log('Datos recibidos:', data);
+
+  const [latitud, longitud, fecha, hora] = data.split(' ');
+
+  
+  console.log('Latitud:', latitud);
+  console.log('Longitud:', longitud);
+  console.log('Fecha:', fecha);
+  console.log('Hora:', hora);
+
+  const query = 'INSERT INTO coordenadas (latitud, longitud, fecha, hora) VALUES (?, ?, ?, ?)';
+
+  const values = [latitud, longitud, fecha, hora];
+  console.log('Query:', query);
+
+  db.query(query, values, (err, results) => {
+    if (err) {
+      console.error('Error al insertar datos en la base de datos:', err);
+    } else {
+      console.log('Datos insertados en la base de datos');
+    }
+  });
+
+  
+  io.emit('datosActualizados', { latitud, longitud, fecha, hora });
+});
+
+udpServer.bind(10001, '0.0.0.0', () => {
+  console.log('Servidor UDP escuchando en el puerto 10001');
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log('Un cliente se ha conectado');
+
+  
+  socket.emit('datosActualizados', ultimaInformacion);
+
+  
+  socket.on('disconnect', () => {
+    console.log('Un cliente se ha desconectado');
+  });
+});
+
+http.listen(4000, '0.0.0.0', () => {
+  console.log('Servidor web escuchando en el puerto 4000');
+});
+
+module.exports = app;
