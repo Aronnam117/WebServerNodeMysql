e149e9bbcea01c2faeb4f50af64fb77237c8949d
Aronnam117 <162920209+Aronnam117@users.noreply.github.com>
2024-03-18 20:04:46 -0500

    Update app.js
    cambio a segundo plano

--- app.js
+++ app.js
@@ -105,7 +105,15 @@ io.on('connection', (socket) => {
 });
 
 http.listen(80, '0.0.0.0', () => {
-  console.log('Servidor web escuchando en el puerto 4000');
+  console.log('Servidor web escuchando en el puerto 80');
 });
 
-module.exports = app;
+// Usar 'forever' para mantener la aplicación en ejecución en segundo plano
+const forever = require('forever');
+const child = new (forever.Monitor)('app.js', {
+  silent: true,
+  args: [],
+});
+
+child.start();
+
