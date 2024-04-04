c36cd86ab6161e80cf8292b35cd34658eb22cde4
Aronnam117 <mateoaronna@hotmail.com>
2024-04-04 02:05:38 -0500

    cambios app.js y html last 4 today

--- app.js
+++ app.js
@@ -1,24 +1,16 @@
-const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const cors = require('cors');
 const mysql = require('mysql2');
-require('dotenv').config();
-
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http, {
   path: '/socket.io',
 });
 
-app.use(cors());
+// Configuración del servidor
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Crear el servidor UDP
-const dgram = require('dgram');
-const udpServer = dgram.createSocket('udp4');
-
+// Creación de la conexión a la base de datos
 const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -26,50 +18,7 @@ const db = mysql.createPool({
   database: process.env.DB_DATABASE,
 });
 
-const ultimaInformacion = {
-  latitud: 0,
-  longitud: 0,
-  fecha: '',
-  hora: '',
-};
-
-udpServer.on('message', (msg, rinfo) => {
-  const data = msg.toString();
-  console.log('Datos recibidos:', data);
-
-  const [latitud, longitud, fecha, hora] = data.split(' ');
-
-  console.log('Latitud:', latitud);
-  console.log('Longitud:', longitud);
-  console.log('Fecha:', fecha);
-  console.log('Hora:', hora);
-
-  const query = 'INSERT INTO coordenadas (latitud, longitud, fecha, hora) VALUES (?, ?, ?, ?)';
-
-  const values = [latitud, longitud, fecha, hora];
-  console.log('Query:', query);
-
-  db.query(query, values, (err, results) => {
-    if (err) {
-      console.error('Error al insertar datos en la base de datos:', err);
-    } else {
-      console.log('Datos insertados en la base de datos');
-    }
-  });
-
-  io.emit('datosActualizados', { latitud, longitud, fecha, hora });
-});
-
-udpServer.bind(10001, '0.0.0.0', () => {
-  console.log('Servidor UDP escuchando en el puerto 10001');
-});
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
-
+// Función para obtener todas las coordenadas almacenadas
 app.get('/coordenadas', (req, res) => {
   const query = 'SELECT * FROM coordenadas'; // Consulta SQL 
   // Ejecutar la consulta en la base de datos
@@ -85,31 +34,35 @@ app.get('/coordenadas', (req, res) => {
   });
 });
 
+// Establecer conexión con los clientes
 io.on('connection', (socket) => {
   console.log('Un cliente se ha conectado');
 
+  // Enviar los datos más recientes al cliente cuando se conecta
   const query = 'SELECT latitud, longitud, fecha, hora FROM coordenadas ORDER BY id DESC LIMIT 1';
-
   db.query(query, (err, results) => {
     if (err) {
       console.error('Error al obtener los datos más recientes de la base de datos:', err);
     } else {
       if (results.length > 0) {
-        ultimaInformacion.latitud = results[0].latitud;
-        ultimaInformacion.longitud = results[0].longitud;
-        ultimaInformacion.fecha = results[0].fecha;
-        ultimaInformacion.hora = results[0].hora;
-
+        const ultimaInformacion = {
+          latitud: results[0].latitud,
+          longitud: results[0].longitud,
+          fecha: results[0].fecha,
+          hora: results[0].hora,
+        };
         socket.emit('datosActualizados', ultimaInformacion);
       }
     }
   });
 
+  // Manejar desconexión de clientes
   socket.on('disconnect', () => {
     console.log('Un cliente se ha desconectado');
   });
 });
 
+// Iniciar el servidor HTTP
 http.listen(80, '0.0.0.0', () => {
   console.log('Servidor web escuchando en el puerto 80');
 });
@@ -118,7 +71,7 @@ http.listen(80, '0.0.0.0', () => {
 app.get('/filtrarRutas', (req, res) => {
   const { fechaInicio, horaInicio, fechaFin, horaFin } = req.query;
 
-  const query = `SELECT latitud, longitud FROM coordenadas WHERE fecha >= ? AND hora >= ? AND fecha <= ? AND hora <= ?`;
+  const query = `SELECT latitud, longitud FROM coordenadas WHERE (fecha = ? AND hora >= ?) OR (fecha > ? AND fecha < ?)`;
   const values = [fechaInicio, horaInicio, fechaFin, horaFin];
 
   db.query(query, values, (err, results) => {
@@ -132,6 +85,5 @@ app.get('/filtrarRutas', (req, res) => {
     res.json(results);
   });
 });
-
 
 module.exports = app;
