25689c870100bba051fb6c0a18cbc45f3e1dce74
Aronnam117 <mateoaronna@hotmail.com>
2024-04-09 01:05:18 -0500

    cambio palabra historial en historial.js por /

--- routes/historial.js
+++ routes/historial.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var path = require('path');
 
 // GET historial page
-router.get('/historial', function(req, res, next) {
+router.get('/', function(req, res, next) {
   // Asegúrate de que la ruta al archivo 'historial.html' sea correcta
   res.sendFile(path.join(__dirname, '..', 'public', 'historial.html'));
 });
